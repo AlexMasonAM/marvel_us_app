b02d7bc429ecd589289bd19b2b3edbebfa9b9760
AlexMasonAM <alexmmason@gmail.com>
2015-02-28 19:23:08 -0800

    updated key

--- app/controllers/api/wordniks_controller.rb
+++ app/controllers/api/wordniks_controller.rb
@@ -3,7 +3,7 @@ module Api
 
     def index
       @wordnik_variable = "hammer";  
-      wordnik_key = "65ba378ed11d1ca3e940f001a620c978e87cfc704ecce75be";
+      wordnik_key = "";
       @resp = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{@wordnik_variable}/relatedWords?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=#{wordnik_key}")
       @rhyme = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=rhyme&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{wordnik_key}")
       @synonym = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{wordnik_key}")
