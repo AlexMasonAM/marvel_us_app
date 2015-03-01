f1a483e7f27d3c06ace5f0498393626e2e674039
AlexMasonAM <alexmmason@gmail.com>
2015-02-28 19:54:13 -0800

    updated key

--- app/controllers/api/wordniks_controller.rb
+++ app/controllers/api/wordniks_controller.rb
@@ -2,12 +2,12 @@ module Api
   class WordniksController < ApplicationController 
 
     def index
-      @wordnik_variable = "hammer";  
-      wordnik_key = "";
-      @resp = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{@wordnik_variable}/relatedWords?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=#{wordnik_key}")
-      @rhyme = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=rhyme&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{wordnik_key}")
-      @synonym = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{wordnik_key}")
-      @defined = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=#{wordnik_key}")
+      @wordnik_variable = "drum";  
+  
+      @resp = HTTParty.get("http://api.wordnik.com:80/v4/word.json/#{@wordnik_variable}/relatedWords?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=#{ENV['WORDNIK_KEY']}")
+      @rhyme = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=rhyme&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{ENV['WORDNIK_KEY']}")
+      @synonym = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{ENV['WORDNIK_KEY']}")
+      @defined = HTTParty.get("http://api.wordnik.com/v4/word.json/#{@wordnik_variable}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=#{ENV['WORDNIK_KEY']}")
 
       render json: @resp
     end
